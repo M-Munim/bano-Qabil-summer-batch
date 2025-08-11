047f8788ac45ca819137b60b4d9bcc991f4a8a19
M-Munim <muhammadmunimoff330@gmail.com>
2025-08-11 16:42:10 +0500

    js day 1

---
+++ JsD1/script.js
@@ -0,0 +1,37 @@
+// console.log("hello");
+
+// alert("Hello munim")
+
+// prompt("Hello munim")
+
+
+// let fullName = "M Munim";
+// let age = "20";
+// console.log(typeof Number(fullName));
+
+// document.writeln("Hello Developers");
+
+
+
+let midNumbers = 30;
+let finalNumbers = 70;
+
+console.log(midNumbers + finalNumbers); //100
+console.log(midNumbers - finalNumbers); // -40
+console.log(midNumbers * finalNumbers); //100
+console.log(midNumbers / finalNumbers); //100
+console.log(midNumbers % finalNumbers); //100
+
+
+let num = 20;
+console.log(num);
+num++;
+console.log(num);
+
+let discount = 500;
+
+let total = 6000;
+// let a = aNum +=  7;
+let finalTotal = total -= discount;
+
+console.log(finalTotal);
